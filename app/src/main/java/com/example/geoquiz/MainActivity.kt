2abeeac1f05ad15febdd9336141cbd60c2116a50
alibek.nurgaliyev <alibek.nurgaliyev@gmail.com>
2021-11-22 11:31:08 +0600

    Added total counter

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.geoquiz
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -14,19 +18,22 @@ class MainActivity : AppCompatActivity() {
 
 
     private val questionBank = listOf(
-            Question(R.string.question_australia, true),
-            Question(R.string.question_oceans, true),
-            Question(R.string.question_mideast, false),
-            Question(R.string.question_africa, false),
-            Question(R.string.question_americas, true),
-            Question(R.string.question_asia, true),
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
     )
 
+    private var correctAnswerNumber = 0
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -52,7 +59,7 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-        prevButton.setOnClickListener{
+        prevButton.setOnClickListener {
             currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
         }
@@ -61,18 +68,64 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun updateQuestion(){
+    private fun updateQuestion() {
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer : Boolean){
+    private fun checkAnswer(userAnswer: Boolean) {
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
         val correctAnwer = questionBank[currentIndex].answer
-        val messageResId = if(userAnswer == correctAnwer){
-            R.string.correct_toast
-        }else{
-            R.string.incorrect_toast
+        var messageResId : Int = 0
+
+        if (userAnswer == correctAnwer){
+            messageResId =R.string.correct_toast
+            correctAnswerNumber++
         }
+        else {
+            messageResId = R.string.incorrect_toast
+        }
+//        val messageResId = if (userAnswer == correctAnwer) {
+//            R.string.correct_toast
+//        } else {
+//            R.string.incorrect_toast
+//        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        if(currentIndex+1 == questionBank.size){
+            val total  = ((correctAnswerNumber.toDouble()/questionBank.size)*100).toInt()
+            Toast.makeText(this, "Total result: $total %", Toast.LENGTH_LONG).show()
+            correctAnswerNumber = 0
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+
 }
