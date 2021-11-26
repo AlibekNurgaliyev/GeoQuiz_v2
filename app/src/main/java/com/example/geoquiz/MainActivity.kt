73cba2ea65fa72cc54fd5e119bedc9a53d9b4d91
alibek.nurgaliyev <alibek.nurgaliyev@gmail.com>
2021-11-26 18:38:56 +0600

    Added viewModel

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.geoquiz
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -16,24 +19,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true),
-    )
-
-    private var correctAnswerNumber = 0
-    private var currentIndex = 0
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.i(TAG,"onSaveInstanceState called")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0)?:0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -55,12 +57,12 @@ class MainActivity : AppCompatActivity() {
 
 
         nextButton.setOnClickListener { view: View ->
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
         prevButton.setOnClickListener {
-            currentIndex = (currentIndex - 1) % questionBank.size
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
 
@@ -71,35 +73,32 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         trueButton.isEnabled = true
         falseButton.isEnabled = true
-        val questionTextResId = questionBank[currentIndex].textResId
+
+        val questionTextResId=quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         trueButton.isEnabled = false
         falseButton.isEnabled = false
-        val correctAnwer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         var messageResId : Int = 0
 
-        if (userAnswer == correctAnwer){
+        if (userAnswer == correctAnswer){
             messageResId =R.string.correct_toast
-            correctAnswerNumber++
+//            quizViewModel.correctAnswerNumber++
         }
         else {
             messageResId = R.string.incorrect_toast
         }
-//        val messageResId = if (userAnswer == correctAnwer) {
-//            R.string.correct_toast
-//        } else {
-//            R.string.incorrect_toast
-//        }
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
 
-        if(currentIndex+1 == questionBank.size){
-            val total  = ((correctAnswerNumber.toDouble()/questionBank.size)*100).toInt()
-            Toast.makeText(this, "Total result: $total %", Toast.LENGTH_LONG).show()
-            correctAnswerNumber = 0
-        }
+//        if(quizViewModel.currentIndex+1 == questionBank.size){
+//            val total  = ((correctAnswerNumber.toDouble()/questionBank.size)*100).toInt()
+//            Toast.makeText(this, "Total result: $total %", Toast.LENGTH_LONG).show()
+//            correctAnswerNumber = 0
+//        }
     }
 
     override fun onStart() {
